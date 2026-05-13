1bffbda9e445f7d5230958064fd833c4eeb34ffe
ragul78914-bit <ragul78914@gamil.com>
2026-05-13 13:18:27 +0530

    updated project

--- nextjs-app/src/lib/firebase.js
+++ nextjs-app/src/lib/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCYrnEE4jmO6BBCrBRn0MEpiDLkHB50FQg",
-  authDomain: "constra-efd4d.firebaseapp.com",
-  projectId: "constra-efd4d",
-  storageBucket: "constra-efd4d.firebasestorage.app",
-  messagingSenderId: "197431773308",
-  appId: "1:197431773308:web:24977a6d0ef9b0707461f9",
-  measurementId: "G-WSKLLEN21K"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
